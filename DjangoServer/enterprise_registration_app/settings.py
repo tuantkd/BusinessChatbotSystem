d54ea615cb5de317d639238a2cfc6d094f280fe3
minhdua <vothanhhienag1996@gmail.com>
2024-02-19 19:19:25 +0700

    "Remove specific lines from business_code.txt" "Comment out APPEND_SLASH in settings.py" "Change STATIC_URL path in settings.py"

--- DjangoServer/enterprise_registration_app/settings.py
+++ DjangoServer/enterprise_registration_app/settings.py
@@ -120,13 +120,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-APPEND_SLASH = False
+# APPEND_SLASH = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'chat/static/'
-
+# STATIC_URL = 'chat/static/'
+STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
